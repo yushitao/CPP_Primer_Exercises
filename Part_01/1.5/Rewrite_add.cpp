4b18afb8c40838f883eff1b1eb2d05b63cc743e5
Yu <shitao.yu@nexteer.com>
2022-04-25 15:23:23 +0800

    add exercise 1.5!

---
+++ Part_01/1.5/Rewrite_add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y;
+	cout<<"Please input the x,y!"<<endl;
+	cin>>x>>y;
+	cout<<"the multiplication between:";
+	cout<<x;
+	cout<<" and ";
+	cout<<y;
+	cout<<" is ";
+	cout<<x*y;
+	cout<<endl;
+}
