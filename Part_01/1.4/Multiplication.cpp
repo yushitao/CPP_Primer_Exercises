97d623be4b46c36e0e546936ed518d97c94a608a
Yu <shitao.yu@nexteer.com>
2022-04-25 15:04:26 +0800

    add exercise 1.4!

---
+++ Part_01/1.4/Multiplication.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y;
+	cout<<"Please input the x,y!"<<endl;
+	cin>>x>>y;
+	cout<<"the multiplication between:"<<x<<" and "<<y
+		<<" is "<<x*y<<endl;
+}
