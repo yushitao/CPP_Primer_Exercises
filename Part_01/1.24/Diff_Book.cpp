6305bf7278ed002b30497c7700458b02752d71b9
Yu <shitao.yu@nexteer.com>
2022-10-21 11:16:05 +0800

    update 1.24

--- Part_01/1.24/Diff_Book.cpp
+++ Part_01/1.24/Diff_Book.cpp
@@ -5,14 +5,29 @@ using namespace std;
 
 int main()
 {
-	Sales_item Book,Sum;
+	bool FindBook = false;
+	int TypeCount = 0;
+	Sales_item Book,Books[5];
 	cout<<"Please Input Books Info:"<<endl;
 	for(int i=0;i<5;i++)
 	{
 		cin>>Book;
-		Sum+=Book;
+		for(int j=0;j<TypeCount;j++)
+		{
+			if(Books[j].same_isbn(Book))
+			{
+				Books[j]+=Book;
+				FindBook = true;
+				break;
+			}
+		}
+		if(!FindBook)
+			Books[TypeCount++] = Book;
+		FindBook = false;
 	}
-
-		cout<<"Result is:"<<Sum<<endl;
+	for(int i=0;i<TypeCount;i++)
+	{
+		cout<<Books[i]<<endl;
+	}
 	return 0;
 }
